a2f7fd6866114b16b9974200dd03e025227396c9
YellowFlash1901 <vardaansathe@gmail.com>
2025-05-29 13:48:35 +0100

    made expense model file also made user model file.

---
+++ expense-tracker-backend/src/main/java/com/example/expense_tracker_backend/models/UserModel.java
@@ -0,0 +1,16 @@
+package com.example.expense_tracker_backend.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+}
